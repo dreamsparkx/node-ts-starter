8b38304ae7af5fef94830545b2117e4ac0730b03
Gaurav Bharti <dreamsparks@outlook.com>
2022-04-25 23:06:08 +0530

    chore(package): updated package jest

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "js"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest/legacy",
   },
   testMatch: ["**/test/**/*.test.(ts|js)"],
   testEnvironment: "node",
