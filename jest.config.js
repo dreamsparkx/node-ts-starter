9c464b77e9f14c0a25da28a52bd6db511ff8f704
Gaurav Bharti <dreamsparks@outlook.com>
2022-05-03 23:34:46 +0530

    chore(jest): removed legacy transformer

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "js"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest/legacy",
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
   testMatch: ["**/test/**/*.test.(ts|js)"],
   testEnvironment: "node",
