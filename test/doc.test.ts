4dab2f37f6c6f1d60eabab74a91ad514f0fdf8c3
Gaurav Bharti <dreamsparks@outlook.com>
2022-07-12 22:53:09 +0530

    chore(doc): added tests for doc urls

---
+++ test/doc.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("GET /api-docs", () => {
+  it("should return 301 Moved Permanently", (done) => {
+    request(app).get("/api-docs").expect(301, done);
+  });
+});
+
+describe("GET /swagger.json", () => {
+  it("should return 200 OK", (done) => {
+    request(app)
+      .get("/swagger.json")
+      .expect(200)
+      .expect(function (res) {
+        const contentType = res.header["content-type"];
+        expect(contentType).toBe("application/json; charset=utf-8");
+      })
+      .end(done);
+  });
+});
+
+describe("GET /redoc", () => {
+  it("should return 200 OK", (done) => {
+    request(app).get("/redoc").expect(200, done);
+  });
+});
