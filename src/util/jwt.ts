467840b70aca22a68afb01701e31f43248992439
Gaurav Bharti <dreamsparks@outlook.com>
2025-04-13 18:46:21 +0530

    chore(updates): updated deps

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { ACCESS_TOKEN_SECRET } from "./secrets";
+import type { StringValue } from "ms";
 
 /**
  * Returns signedJWT
@@ -14,7 +15,7 @@ import { ACCESS_TOKEN_SECRET } from "./secrets";
 
 export const generateJWT = (
   toBeSignedObj: string | Buffer | object,
-  expiresIn?: string | number | undefined,
+  expiresIn?: StringValue | number,
 ) => {
   return jwt.sign(toBeSignedObj, ACCESS_TOKEN_SECRET, {
     expiresIn: expiresIn || "1800s",
